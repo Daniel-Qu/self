700a27e06e19e468440173708aa6a00ec5ae6185
daniel <qushuai947@163.com>
2018-07-18 08:14:33 +0800

    add C++ Primer(5th): exercise 6.7 ~P185; exercise 6.8 ~P186

---
+++ C++ Primer/chapter6/exercise/6.8/6.8.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+
+int main(int argc, char *argv[])
+{
+    char key;
+    while(std::cin >> key && key != 'q')
+    {
+        std::cout << "static_calls: " << static_calls() << std::endl;
+        std::cout << "calls: " << calls() << std::endl;
+    }
+    return 0;
+}
+int static_calls()
+{
+    static int ansower = 0;
+    if(ansower++ == 0)
+        return 0;
+    else
+        return 1;
+}
+int calls()
+{
+    int ansower = 0;
+    if(ansower++ == 0)
+        return 0;
+    else
+        return 1;
+}
---
+++ C++ Primer/chapter6/exercise/6.8/6.8.h
@@ -0,0 +1,7 @@
+#ifndef STATIC_TEST_H
+#define STATIC_TEST_H
+
+int static_calls();
+int calls();
+
+#endif
